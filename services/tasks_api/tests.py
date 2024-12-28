6198a148e2eb9f57dc85ba4ecb2a9e2dfb409116
Abel Huanca <abel.huanca@upeu.edu.pe>
2024-12-28 17:15:43 -0500

    Add code quality job

--- services/tasks_api/tests.py
+++ services/tasks_api/tests.py
@@ -4,9 +4,11 @@ from starlette.testclient import TestClient
 
 from main import app
 
+
 @pytest.fixture()
 def client():
     return TestClient(app)
+
 
 def test_health_check(client):
     """
@@ -15,8 +17,6 @@ def test_health_check(client):
     THEN response with status 200 and body OK is returned
     """
 
-    response =client.get("/api/health-check/")
+    response = client.get("/api/health-check/")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "OK"}
-
-
